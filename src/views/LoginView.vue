4aabf4a2aed1f36f0b7144f22c8102ad8eafde65
Arcgel <darkdusty8000@gmail.com>
2025-05-05 00:39:44 +0800

    Log In and Register Path

---
+++ src/views/LoginView.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="auth-container row">
+    <!-- Left Side -->
+    <div class="col-md-6 split-left text-center">
+      <img src="@/assets/Images/books.gif" alt="Library Illustration" class="illustration mb-4" />
+    </div>
+
+    <!-- Right Side -->
+    <div class="col-md-6 d-flex align-items-center split-right">
+      <div class="w-100" style="max-width: 350px; margin: auto">
+        <h2 class="mb-3 text-center">Library</h2>
+        <p class="text-center text-muted">Welcome to the Books Store</p>
+
+        <div class="mb-3">
+          <label class="form-label">Email</label>
+          <input type="text" class="form-control" placeholder="Enter your email" />
+        </div>
+
+        <div class="mb-2">
+          <label class="form-label">Password</label>
+          <input type="password" class="form-control" placeholder="Enter your password" />
+        </div>
+
+        <div class="d-grid mt-3">
+          <button class="btn btn-signin">Log in</button>
+        </div>
+
+        <div class="text-center my-3 text-muted">or</div>
+
+        <p class="text-center link">Don't have an account yet?
+          <a href="#" class="create">Create Account</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.auth-container {
+  max-width: 900px;
+  margin: 50px auto;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+}
+
+.split-left {
+  background: linear-gradient(to right, #808994, #3c729e);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  color: white;
+}
+
+.split-right {
+  padding: 3rem;
+}
+
+.illustration {
+  max-width: 250px;
+  height: auto;
+}
+
+.btn-signin {
+  background-color: #555;
+  color: white;
+  border-radius: 15px;
+  width: 100%;
+}
+
+.form-control {
+  border-bottom: 1px solid #e7e0e0;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  text-align: left;
+}
+
+.link {
+  font-size: 0.9rem;
+}
+
+.form-control:focus {
+  border-color: #ccc;
+  box-shadow: none;
+  outline: none;
+}
+
+.create {
+  text-decoration: underline;
+}
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,131 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <body>
+    <div class="auth-container row">
+      <div class="col-md-6 d-flex align-items-center split-right">
+        <div class="w-100" style="max-width: 350px; margin: auto">
+          <p class="create text-center text-muted" style="font-family: Times New Roman, Times, serif;">Create Account
+          </p>
+
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
+
+          <!-- Name -->
+          <div class="mb-3 position-relative">
+            <i class="bi bi-person position-absolute top-50 start-0 translate-middle-y ms-3 text-muted"></i>
+            <input type="text" class="form-control ps-5 border rounded" placeholder="Name" />
+          </div>
+
+          <!-- Email -->
+          <div class="mb-3 position-relative">
+            <i class="bi bi-envelope position-absolute top-50 start-0 translate-middle-y ms-3 text-muted"></i>
+            <input type="text" class="form-control ps-5 border rounded" placeholder="Email" />
+          </div>
+
+          <!-- Password -->
+          <div class="mb-3 position-relative">
+            <i class="bi bi-lock position-absolute top-50 start-0 translate-middle-y ms-3 text-muted"></i>
+            <input type="password" class="form-control ps-5 border rounded" placeholder="Password" />
+          </div>
+
+          <!-- Phone -->
+          <div class="mb-3 position-relative">
+            <i class="bi bi-telephone position-absolute top-50 start-0 translate-middle-y ms-3 text-muted"></i>
+            <input type="tel" class="form-control ps-5 border rounded" placeholder="Phone number" />
+          </div>
+
+
+
+          <div class="d-grid mt-3">
+            <button class="btn btn-signin" style="font-family: Times New Roman, Times, serif;">Register</button>
+          </div>
+
+          <div class="text-center my-3 text-muted" style="font-family: Times New Roman, Times, serif;">or</div>
+
+          <p class="text-center link"> If you already have an account
+            <a href="#" class="sign-in" style="font-family: Times New Roman, Times, serif;">Sign in</a>
+          </p>
+        </div>
+      </div>
+      <!-- Left Side -->
+      <div class="col-md-6 split-left text-center">
+        <img src="../assets/Images/reading.gif" alt="Bird Illustration" class="illustration mb-4" />
+      </div>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+body,
+html {
+  margin: 0;
+  font-family: "Segoe UI", sans-serif;
+  background-color: #f7f7f7;
+}
+
+.auth-container {
+  max-width: 800px;
+  height: 490px;
+  margin: 200px auto;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+}
+
+.split-left {
+  background: linear-gradient(to right, #808994, #3c729e);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  color: white;
+}
+
+.split-right {
+  padding: 3rem;
+}
+
+.illustration {
+  max-width: 250px;
+  height: auto;
+}
+
+.btn-signin {
+  background-color: #555;
+  color: white;
+  border-radius: 15px;
+  width: 100%;
+}
+
+.form-control {
+  border: none;
+  border-bottom: 1px solid #e7e0e0;
+  border-radius: 0;
+  margin-bottom: 15px;
+  font-family: "Times New Roman", Times, serif;
+  box-shadow: none;
+}
+
+.link {
+  font-size: 0.9rem;
+}
+
+.form-control:focus {
+  border-color: #ccc;
+  box-shadow: none;
+  outline: none;
+
+}
+
+.create {
+  font-size: 20px;
+  font-weight: bold;
+
+
+}
+</style>
